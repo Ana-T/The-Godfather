f90e9b2af8eba974ebe634773659f09ce7999623
Ana T <anat@10-22-204-173.ed2.openfct.fct.unl.pt>
2013-05-18 12:16:11 +0100

    zxzx

---
+++ iConfs3/iConfs3/Bar.h
@@ -0,0 +1,13 @@
+//
+//  Bar.h
+//  iConfs3
+//
+//  Created by Ana T on 18/05/13.
+//  Copyright (c) 2013 G10PI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Bar : UINavigationBar
+
+@end
---
+++ iConfs3/iConfs3/Bar.m
@@ -0,0 +1,32 @@
+//
+//  Bar.m
+//  iConfs3
+//
+//  Created by Ana T on 18/05/13.
+//  Copyright (c) 2013 G10PI. All rights reserved.
+//
+
+#import "Bar.h"
+
+@implementation Bar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
