acb4ede40781ea1bf95ee5b37624eb6365d809c4
Eduardo Joel Pereira Beja Martins <bejamartins@gmail.com>
2013-05-20 18:30:37 +0100

    Documentation...

--- iConfs3/iConfs3/News.h
+++ iConfs3/iConfs3/News.h
@@ -17,13 +17,40 @@
     NSDate* sentDate;
 }
 
+//News title
 @property (nonatomic) NSString* title;
+
+//News text
 @property (nonatomic) NSString* text;
+
+//News sent date
 @property (nonatomic) NSDate* sentDate;
 
+/**
+ Initializes a new News object
+ @param t title of the news
+ @param txt text of the news
+ @param date sent date of the news
+ @returns a newly initialized object
+ */
 -(News*)initWithData: (NSString*) t text: (NSString*) txt date:(NSDate*)date;
+
+/**
+ Gets the news title
+ @returns news title
+ */
 -(NSString*)getTitle;
+
+/**
+ Gets the news text
+ @returns news text
+ */
 -(NSString*)getText;
+
+/**
+ Gets the news date
+ @returns news date
+ */
 -(NSDate*)getDate;
 
 @end
