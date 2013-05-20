244f6c0bdf2f7ee9ff2b66de061c26aa65ba9ecc
Eduardo Joel Pereira Beja Martins <bejamartins@gmail.com>
2013-05-20 17:15:22 +0100

    Event.m

--- iConfs3/Event.h
+++ iConfs3/Event.h
@@ -11,7 +11,7 @@
 
 @interface Event : NSObject
 {
-@protected
+    @protected
     NSDate* date;
     NSString* title;
     NSString* theme;
@@ -21,27 +21,120 @@
     int rating;
     int eventID;
 }
-//@property (nonatomic) NSDate* date;
-//@property (nonatomic) NSString* title;
-//@property (nonatomic) NSString* theme;
-//@property (nonatomic) Speaker* speaker;
-//@property (nonatomic) BOOL userVoted;
-//@property (nonatomic) int vote;
-//@property (nonatomic) int rating;
-//@property (nonatomic) int eventID;
 
+//Date and time of the event
+@property (nonatomic) NSDate* date;
+
+//Title of the event
+@property (nonatomic) NSString* title;
+
+//Theme of the event
+@property (nonatomic) NSString* theme;
+
+//Speaker of the event
+@property (nonatomic) Speaker* speaker;
+
+//Check if the user has voted
+@property (nonatomic) BOOL userVoted;
+
+//Rating of the event by the user
+@property (nonatomic) int vote;
+
+//Overall rating of the event
+@property (nonatomic) int rating;
+
+//ID of the event
+@property (nonatomic) int eventID;
+
+/**
+ Initializes a new Event object
+ @param eID id of the event
+ @param d date and time of the conference
+ @param t title of the event
+ @param th theme of the event
+ @returns a newly initialized object
+ */
 -(Event*)initWithData:(int)eID date:(NSDate*)d title:(NSString*)t theme:(NSString*)th;
+
+/**
+ Initializes a new Event object with Speaker
+ @param eID id of the event
+ @param d date and time of the conference
+ @param t title of the event
+ @param th theme of the event
+ @param s spearker of the event
+ @returns a newly initialized object
+ */
 -(Event*)initWithDataAndSpeaker:(int)eID date:(NSDate*)d title:(NSString*)t theme:(NSString*)th speaker: (Speaker*)s;
--(void)vote:(int)vote;
+
+/**
+ Preforms a user vote
+ @param v user vote
+ @returns
+ */
+-(void)vote:(int)v;
+
+/**
+ Gets event title
+ @returns event's title
+ */
 -(NSString*)getTitle;
+
+/**
+ Gets event ID
+ @returns event's ID
+ */
 -(int)getID;
+
+/**
+ Gets event theme
+ @returns event's theme
+ */
 -(NSString*)getTheme;
+
+/**
+ Gets event speaker
+ @returns event's speaker
+ */
 -(Speaker*)getSpeaker;
+
+/**
+ Sets event speaker
+ @param s new event speaker
+ @returns 
+ */
 -(void)setSpeaker:(Speaker*)s;
+
+/**
+ Gets user's vote
+ @returns user's vote or -1 if user hasn't voted on the event
+ */
 -(int)getVote;
+
+/**
+ Inserts overal event rating
+ @param r rating
+ @returns 
+ */
 -(void)rate:(int)r;
--(void)getRate;
+
+/**
+ Gets event's overall rating
+ @returns rating or -1 if there are no votes on the event
+ */
+-(int)getRate;
+
+/**
+ Gets event's overall rating
+ @returns rating or -1 if there are no votes on the event
+ */
 -(NSDate*)getDate;
+
+/**
+ Sets event's date and tume
+ @param d nem date and time
+ @returns 
+ */
 -(void)setDate:(NSDate*)d;
 
 
--- iConfs3/Event.m
+++ iConfs3/Event.m
@@ -10,4 +10,69 @@
 
 @implementation Event
 
+-(Event*)initWithData:(int)eID date:(NSDate*)d title:(NSString*)t theme:(NSString*)th{
+    self.eventID = eID;
+    self.date = d;
+    self.title = t;
+    self.theme = th;
+    self.vote = -1;
+    self.rating = -1;
+    return self;
+}
+
+-(Event*)initWithDataAndSpeaker:(int)eID date:(NSDate*)d title:(NSString*)t theme:(NSString*)th speaker: (Speaker*)s{
+    self.eventID = eID;
+    self.date = d;
+    self.title = t;
+    self.theme = th;
+    self.speaker = s;
+    self.vote = -1;
+    self.rating = -1;
+    return self;
+}
+
+-(void)vote:(int)v{
+    self.vote = v;
+}
+
+-(NSString*)getTitle{
+    return title;
+}
+
+-(int)getID{
+    return eventID;
+}
+
+-(NSString*)getTheme{
+    return theme;
+}
+
+-(Speaker*)getSpeaker{
+    return speaker;
+}
+
+-(void)setSpeaker:(Speaker*)s{
+    self.speaker = s;
+}
+
+-(int)getVote{
+    return vote;
+}
+
+-(void)rate:(int)r{
+    self.rating = r;
+}
+
+-(int)getRate{
+    return rating;
+}
+
+-(NSDate*)getDate{
+    return date;
+}
+
+-(void)setDate:(NSDate*)d{
+    self.date = d;
+}
+
 @end
